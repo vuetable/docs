0ea1650789a4ef6167ab63676890232b3cbfcac3
Rati Wannapanop <rati.wannapanop@gmail.com>
2018-06-01 13:23:14 +0700

    update repo link to next branch

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
     //['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css' }]
   ],
   themeConfig: {
-    repo: 'ratiw/vuetable-2',
+    repo: 'ratiw/vuetable-2/tree/next',
     locales: {
       '/': {
         nav: [
