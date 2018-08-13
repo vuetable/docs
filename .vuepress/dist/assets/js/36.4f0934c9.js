2e9e9bde39da41f512a7b443c97f168dfcdddf39
Rati Wannapanop <rati.wannapanop@gmail.com>
2018-08-13 18:10:13 +0700

    update docs

--- .vuepress/dist/assets/js/36.4f0934c9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{184:function(n,t,e){"use strict";e.r(t);var s=e(0),a=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("span",[this._v("\n  2.0.0-beta.1\n")])},[],!1,null,null,null);t.default=a.exports}}]);
---
+++ .vuepress/dist/assets/js/36.508aa9ef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{156:function(n,t,e){"use strict";e.r(t);var s=e(0),a=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("span",[this._v("\n  2.0.0-beta.1\n")])},[],!1,null,null,null);t.default=a.exports}}]);
